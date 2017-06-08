72cf98dc1d26c067ce6278a2ca392960a30f060a
AUN <xitmktod@gmail.com>
2017-06-08 13:26:24 -0700

    aun

--- app/src/main/java/com/example/aun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aun/myapplication/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.aun.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    public void showText(View view){
+        String msg ="I'm Handsome";
+        Toast toast = Toast.makeText(this,msg,Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
